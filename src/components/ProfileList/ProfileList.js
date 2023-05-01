21090596bdfde5966d1b452161049b9f1233c948
Andrew <papsujevicandrew@gmail.com>
2023-05-01 17:59:09 +0300

    Path fix #2

--- src/components/ProfileList/ProfileList.js
+++ src/components/ProfileList/ProfileList.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Profile } from 'components/Profile/Profile';
+import { Profile } from '../Profile/Profile';
 import { List, Item } from './ProfileList.styled';
 export const ProfileList = ({ items }) => {
   return (
